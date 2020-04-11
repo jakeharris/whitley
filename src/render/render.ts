6738a97563004e7ad21e4ad790f03f1010ad94bd
Jake Harris <milieu@javakat.io>
2020-04-10 19:22:12 -0500

    Back the camera up so we can see stuff

--- src/render/render.ts
+++ src/render/render.ts
@@ -20,6 +20,8 @@ export function createThreeRenderTools(): ThreeRenderTools {
     0.1,
     1000
   );
+  camera.position.set(0, 0, 10);
+
   const renderer = new Three.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
 
