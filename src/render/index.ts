043413eade769e58144adf837383761d1459617d
Jake Harris <milieu@javakat.io>
2020-04-10 19:23:29 -0500

    Bundle stuff

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,5 +1,7 @@
 import * as Render from "./render";
 import * as Object from "./object";
 import * as Cube from "./cube";
+import * as Litly from "./litly";
+import * as Light from "./light";
 
-export { Render, Object, Cube };
+export { Render, Object, Cube, Litly, Light };
